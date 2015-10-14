2a9a4e7c5be30eade11fcb64b234624124d5bfd4
lys623 <lys623@qq.com>
2015-10-14 11:06:37 +0800

    修改模版位置

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ exports.options = {
     '-u, --upload':'upload a project(css,js,img) to serverDir',
     '-uh, -u --html':'upload a project(html) to previewServerDir',
     '-uc, -u --css':'upload a project(css,js,img) to serverDir',
+    '--nodir':'create a project to cur dir'
 };
 var jdf=require('./lib/jdf.js')
 exports.run = function(argv, cli, env) {
@@ -25,7 +26,7 @@ exports.run = function(argv, cli, env) {
             return ;
         }
         require('./modal/upload.js').init(env.configBase,argv);
-    }else if(argv.n||argv.name){
+    }else if(argv.n||argv.name||argv.nodir){
         jdf.createStandardDir(porjectName,argv);
     }else{
         return cli.help(exports.name, exports.options);
@@ -37,7 +38,7 @@ exports.run = function(argv, cli, env) {
             return;
         }
 
-        var allowed = ['_', 'name', 'n', 'upload', 'u', 'html', 'h','p','m','c'];
+        var allowed = ['_', 'name', 'n', 'upload', 'u', 'html', 'h','p','m','c','nodir'];
 
         Object.keys(argv).forEach(function(k) {
             if (!~allowed.indexOf(k)) {
